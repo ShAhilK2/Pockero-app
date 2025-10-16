1c1556475522da46e594f5e70f3bd6de9871a09e
ShAhilK2 <shahilkataria852002@gmail.com>
2025-10-16 16:57:10 +0530

    02 : Native Tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
-import { Stack } from "expo-router";
+import { COLORS } from "@/utils/Colors";
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <NativeTabs blurEffect="systemChromeMaterial" tintColor={COLORS.textDark}>
+      <NativeTabs.Trigger name="home">
+        <Label>Home</Label>
+        <Icon
+          sf={{ default: "house", selected: "house.fill" }}
+          drawable="ic_home"
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="saves">
+        <Label>Saves</Label>
+        <Icon
+          sf={{ default: "heart", selected: "heart.fill" }}
+          drawable="ic_saves"
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <Label>Settings</Label>
+        <Icon
+          sf={{ default: "gearshape", selected: "gearshape.fill" }}
+          drawable="ic_settings"
+        />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
---
+++ app/home/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+
+const Page = () => {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flex: 1 }}
+      contentInsetAdjustmentBehavior="automatic"
+    >
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+      <Text>Page</Text>
+    </ScrollView>
+  );
+};
+
+export default Page;

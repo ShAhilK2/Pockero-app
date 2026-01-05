13be7479f8771a80200acb42ecdf9f8970932eca
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-01-05 20:34:35 +0530

    feat : Sqlite integration

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ bunx expo customize babel.config.js
 bun add drizzle-orm expo-sqlite@next  
 bun add -D drizzle-kit
 
+bun add drizzle-orm
+
 bun install babel-plugin-inline-import
 
 <!-- After that add plugin  -->
@@ -19,3 +21,7 @@ bun install babel-plugin-inline-import
 plugins: [["inline-import", { "extensions": [".sql"] }]] // <-- add this
 
 bun i expo-drizzle-studio-plugin
+
+bunx expo install expo-crypto
+
+bunx expo install expo-sqlite
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
 import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import { Stack } from "expo-router";
-import React from "react";
+import { openDatabaseSync, SQLiteProvider } from "expo-sqlite";
+import React, { Suspense } from "react";
+import { ActivityIndicator } from "react-native";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+import migrations from "../drizzle/migrations";
+
+const DATABASE_NAME = "pocktica";
 
 const Layout = () => {
   const { isSignedIn } = useAuth();
+  const db = openDatabaseSync(DATABASE_NAME);
+  useDrizzleStudio(db);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -14,7 +25,6 @@ const Layout = () => {
           name="(public)/index"
           options={{ headerShown: false, title: "Login" }}
         />
-
         <Stack.Screen
           name="(public)/verify-code"
           options={{
@@ -29,10 +39,28 @@ const Layout = () => {
 };
 
 const RootLayout = () => {
+  const expoDb = openDatabaseSync(DATABASE_NAME);
+  const db = drizzle(expoDb);
+
+  const { success, error } = useMigrations(db, migrations);
+
+  console.log("success", success);
+  console.log("error", error);
+
+  console.log("Migrations completed successfully ✅🚀");
+
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <KeyboardProvider>
-        <Layout />
+        <Suspense fallback={<ActivityIndicator />}>
+          <SQLiteProvider
+            useSuspense
+            databaseName={DATABASE_NAME}
+            options={{ enableChangeListener: true }}
+          >
+            <Layout />
+          </SQLiteProvider>
+        </Suspense>
       </KeyboardProvider>
     </ClerkProvider>
   );

0f84ce173f7c79c8ae0e74b09b817527571ad5a4
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-01-05 17:33:11 +0530

    feat : Rss Feed using API Routes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 const index = () => {
-  return <Redirect href="/" />;
+  return <Redirect href="/(tabs)/home" />;
 };
 
 export default index;
